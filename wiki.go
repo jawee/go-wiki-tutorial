5dda20aaadc6b1d5d75205bf68d86c4c9717b58c
Andreas Olsson <olsson.andreas@gmail.com>
2019-08-24 16:48:01 +0200

    MOved templates to tmpl-directory Moved data (aka pages) to data-directory

--- wiki.go
+++ wiki.go
@@ -7,18 +7,18 @@ import (
     "net/http"
     "regexp"
     "errors"
+    "fmt"
 )
 
 type Page struct {
     Title string
     Body  []byte
 }
-
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func (p *Page) save() error {
-    filename := p.Title + ".txt"
+    filename := "data/" + p.Title + ".txt"
     return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
@@ -32,6 +32,7 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    fmt.Printf("%s", tmpl)
     err := templates.ExecuteTemplate(w, tmpl+".html", p)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -40,8 +41,6 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        // Here we will extract the page title from the Request,
-        // and call the provided handler 'fn'
         m := validPath.FindStringSubmatch(r.URL.Path)
         if m == nil {
             http.NotFound(w, r)
@@ -52,7 +51,7 @@ func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.Hand
 }
 
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
+    filename := "data/" + title + ".txt"
     body, err := ioutil.ReadFile(filename)
     if err != nil {
         return nil, err
